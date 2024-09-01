73a7b369a2766afb82a8276ab675a13dbb2cd58a
Alvaro Villalba <alvaro.villalbaperez@icloud.com>
2024-09-01 08:42:33 +0200

    alvipe-talent-v3-v1

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-    env: {
-        GROQ_API_KEY: process.env.GROQ_API_KEY,
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    }
+
 };
 
 export default withNextIntl(nextConfig);
